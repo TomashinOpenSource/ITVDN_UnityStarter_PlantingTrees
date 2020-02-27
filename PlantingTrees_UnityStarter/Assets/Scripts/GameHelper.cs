305800f57dcc14ad16133ce070bfa945cbb08396
tomashined <45771896+tomashined@users.noreply.github.com>
2020-02-27 13:27:25 +0300

    Lesson End

---
+++ PlantingTrees_UnityStarter/Assets/Scripts/GameHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHelper : MonoBehaviour
+{
+    int _appleCounter = 0;
+    public Text AppleCounter;
+    public int TreePrice = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                if (hit.collider.gameObject == null)
+                    return;
+                Debug.Log(hit.collider.gameObject.name);
+
+                if (hit.collider.gameObject.name.Contains("Apple"))
+                {
+                    _appleCounter++;
+                    AppleCounter.text = _appleCounter.ToString();
+                    Destroy(hit.collider.gameObject);
+                }
+
+                if (hit.collider.gameObject.name.Contains("Floor") && _appleCounter >= TreePrice)
+                {
+                    _appleCounter -= TreePrice;
+                    Vector3 pointPosition = hit.point;
+                    GameObject tree = Instantiate(Resources.Load("Tree"), pointPosition, Quaternion.identity) as GameObject;
+                }
+            }
+        }
+
+        }
+}
---
+++ PlantingTrees_UnityStarter/Assets/Scripts/TreeHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeHelper : MonoBehaviour
+{
+    public float AppleSpawnTime = 3;
+    public Transform PointSpawn;
+    void Start()
+    {
+        InvokeRepeating("Timer", 3, AppleSpawnTime);
+    }
+
+    void Timer()
+    {
+        Debug.Log("Timer()");
+
+        GetComponent<Animator>().SetTrigger("GetApple");
+        GameObject apple = Instantiate(Resources.Load("Apple"), PointSpawn.position, Random.rotation) as GameObject;
+        apple.GetComponent<Rigidbody>().AddForce(apple.transform.forward * 10);
+        Destroy(apple, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
